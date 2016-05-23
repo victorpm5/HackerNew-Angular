c13bb7aa926ef8b0fdd54038c4dee4b5201778ab
albert <albert.suarez.molgo@est.fib.upc.edu>
2016-05-23 20:02:19 +0200

    create project schema like Heroku article

---
+++ server.js
@@ -0,0 +1,5 @@
+/**
+ * Created by albert on 23/05/16.
+ * This file contains all of our server-side code, which implements our REST API.
+ * It’s written in Node.js, using the Express framework and the MongoDB Node.js driver.
+ */
